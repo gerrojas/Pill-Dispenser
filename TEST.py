ffbbf957de24bd18d9209298e406f51f5cc4c7dd
gerrojas <40014404+gerrojas@users.noreply.github.com>
2018-12-03 21:55:20 -0600

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -6,79 +6,50 @@ import time
 localtime = time.asctime (time.localtime(time.time()))
 print ("Fecha y hora actual:")
 print (localtime)
-def job ():
-   print ("Trabajando...")
-schedule.every(1).minute.do(job)
-schedule.every(1).hour.do(job)
-schedule.every(1).day.at("20:28").do(job)
-schedule.every(5).to(10).days.do(job)
-schedule.every(1).monday.do(job)
-schedule.every(1).wednesday.at("19:08").do(job)
-#Condicion de Trabajando
-while True:
-      schedule.run_pending()
-      time.sleep(1)
+
 #El reloj me registra la hora actual
 import time
 localtime = time.asctime (time.localtime(time.time()))
 print ("WELCOME TO THE PILL DISPENSER :")
 print ("PROYECTO FINAL MICROS")
 print (localtime)
->>>>>>> 242f2b5f59fcad01a679659d1d6334dc7f4ff95d
 #Caledario del Mes actual
 import calendar
-cal= calendar.month (2018,11)
+cal= calendar.month (2018,12)
 print (cal)
-#Proceso inicia
-def mensaje ():
-    print ("Pill Dispenser")
-def job ():
-   print ("Trabajando...")
-def saludo ():
-   print ("Iniciando...")
-
-schedule.every
-schedule.every(1).hour.do(job)
-schedule.every(1).day.at("20:55").do(job)
-schedule.every(5).to(10).days.do(job)
-schedule.every(1).monday.do(job)
-schedule.every(1).wednesday.at("19:08").do(job)
-time.sleep(1)
-
 print("Iniciando Pill Dispenser")
-
 #Proceso de la Interfaz
 time.sleep(1)
 nombre = input ("Ingrese su nombre")
 print ("Bienvenido")
 print (nombre)
-pregunta = input ("Desea algún medicamento")
-pregunta = int(input("1 : pastilla1  2 : pastilla2"))
-if pregunta < 2 :
-    print ("Pastilla1")
-elif pregunta > 1 :
-    print ("Pastilla2")
-Pastilla1 = int (input("Elija la cantidad "))
-if Pastilla1 > 15 :
-   print ("Medicamentos insuficientes")
-elif Pastilla1 < 15 :
-   print ("Entregando medicamento")
-#hace falta pedir una entrada de tiempo de entrega para entregar por medio de un schedule!
+print ()
+import time
+print("Conteste S (sí) o N (no) a las preguntas")
+#Try sirve para detectar errores dependiendo de la condicion que tenga el codigo
+while True:
+    question = input ("Desea algun medicamento?")
+    if question == "S" :
+        pregunta = int(input("1: Pastilla1  2: Pastilla2"))
+        if pregunta==1:
+            print ("Pastilla1")
+            Pastilla1 = int (input("Elija la cantidad "))
+            if Pastilla1 > 15:
+                print ("Medicamentos insuficientes")
+            elif Pastilla1 < 15:
+                print ("Entregando medicamento")
+        elif pregunta==2:
+            print ("Pastilla2")
+            Pastilla2 = int (input("Elija la cantidad "))
+            if Pastilla2 >15:
+                print ("Medicamentos insuficientes")
+            elif Pastilla2<15:
+                print ("Entregando medicamento")
 
-pregunta = input ("Desea algún medicamento")
-pregunta = int(input("1 : pastilla1  2 : pastilla2"))
-if pregunta < 2 :
-    print ("Pastilla1")
-elif pregunta > 1 :
-    print ("Pastilla2")
-
-Pastilla2 = int (input("Elija la cantidad "))
-if Pastilla2 > 20 :
-    print ("Medicamentos insuficientes")
-elif Pastilla2 < 20 :
-#hace falta pedir una entrada de tiempo de entrega para entregar por medio de un schedule!
-#trabajar en pantalla de confirmacion antes de entregar.
-    print ("Entregando medicamento")
+        elif question == "N" :
+        print ("Buen dia!")
+        time.sleep(5)
+    print ()
 
 #Funcionamiento LED
 
